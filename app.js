7b42a545d5a3126de3b439e39f3470c76b57e439
Ian Wu <onlinemad@gmail.com>
2014-05-12 16:15:06 +0800

    Change port to load process.env.PORT. 	modified:   app.js

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ server.get('/list', function(req, res, next) {
   return next();
 });
 
-server.listen(8080, function() {
+var port = process.env.PORT || 8080;
+server.listen(port, function() {
   console.log('%s listening at %s', server.name, server.url);
 });
